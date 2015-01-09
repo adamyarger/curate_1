814ea4329605ae3d4ab2a5496c6c1caef4fc9050
adamyarger <yargeradam@gmail.com>
2015-01-09 14:51:12 -0600

    CRUD abilities for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+
+	auto_html_for :body do
+	    html_escape
+	    image
+	    youtube(:width => 400, :height => 250, :autoplay => false)
+	    soundcloud
+	    link :target => "_blank", :rel => "nofollow"
+	    simple_format
+	end
 end

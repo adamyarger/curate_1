7e930784f2e39dcadc18b4f98ed3dfa71445fc29
adamyarger <yargeradam@gmail.com>
2015-01-13 18:17:59 -0600

    follow system is up and running next stop ajax land

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+	before_action :authenticate_user!   #add only: [:index] etc to block spcific pages from non loggedin users
 
 	def index
 		@users = User.all.order('created_at DESC')
@@ -9,6 +9,20 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 		@posts = @user.posts			#nfor pagnation on posts
 	end
+
+	def following
+	    @title = "Following"
+	    @user  = User.find(params[:id])
+	    @users = @user.following                       #.paginate(page: params[:page])
+	    render 'show_follow'
+	 end
+
+	  def followers
+	    @title = "Followers"
+	    @user  = User.find(params[:id])
+	    @users = @user.followers                      #.paginate(page: params[:page])
+	    render 'show_follow'
+	  end
 
 
 end
